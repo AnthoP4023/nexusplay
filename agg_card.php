30274e40b57336efde7c174230d4b2dc04939c72
AnthoP4023 <anthonyp200330@gmail.com>
2025-09-19 02:55:43 -0500

    start commit added

---
+++ agg_card.php
@@ -0,0 +1,27 @@
+<?php
+include 'controladores/cont_agg_card.php'; 
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NexusPlay - Tienda de Videojuegos</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/agg_card.css">
+    <link rel="stylesheet" href="assests/fontawesome/css/all.min.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+
+    
+
+    <?php include 'includes/footer.php'; ?>
+
+    
+</body>
+</html>
